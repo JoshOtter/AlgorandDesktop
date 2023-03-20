85425e09a964fba9c5f4f957447353c7440aeb63
JoshOtter <87107050+JoshOtter@users.noreply.github.com>
2023-03-20 10:17:31 -0500

    Added onhover effect for asset item inventory

--- Assets/Scripts/AssetHandler.cs
+++ Assets/Scripts/AssetHandler.cs
@@ -3,13 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class AssetHandler : MonoBehaviour
+public class AssetHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public AssetObject assetObject;
     public TextMeshProUGUI assetName;
     public Image assetImage;
+    public GameObject hoverObject;
 
     public void InitializeAsset(Sprite sprite, Asset asset, ulong amount)
     {
@@ -17,5 +19,15 @@ public class AssetHandler : MonoBehaviour
 
         assetName.text = assetObject.assetName;
         assetImage.sprite = assetObject.assetImage;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        hoverObject.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        hoverObject.SetActive(false);
     }
 }
